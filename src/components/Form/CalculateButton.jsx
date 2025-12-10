d27c63ca45604e2c01767e8c9727eb31bb498566
Adesh Katiya <adeshkatiya27@gmail.com>
2025-12-10 14:03:20 +0530

    style: fix padding in calculate button

--- src/components/Form/CalculateButton.jsx
+++ src/components/Form/CalculateButton.jsx
@@ -2,7 +2,7 @@ const CalculateButton = () => {
     return (
         <button
             type="button"
-            className="flex h-15 cursor-pointer items-center justify-center gap-4 rounded-full bg-lime font-bold text-slate-900 transition-colors duration-300 ease-in-out hover:bg-lime/50 lg:self-start lg:px-10 lg:py-4"
+            className="flex h-15 cursor-pointer items-center justify-center gap-4 rounded-full bg-lime font-bold text-slate-900 transition-colors duration-300 ease-in-out hover:bg-lime/50 lg:self-start lg:px-10"
         >
             <img src="/assets/images/icon-calculator.svg" alt="Calculator" />
             Calculate Repayments
